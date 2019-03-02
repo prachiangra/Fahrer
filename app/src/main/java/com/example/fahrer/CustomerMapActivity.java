7327c14c23db31a1679acf37e9712a111a859593
prachiangra <41967354+prachiangra@users.noreply.github.com>
2019-03-02 10:40:33 +0530

    Update CustomerMapActivity.java

--- app/src/main/java/com/example/fahrer/CustomerMapActivity.java
+++ app/src/main/java/com/example/fahrer/CustomerMapActivity.java
@@ -436,7 +436,7 @@ public class CustomerMapActivity extends FragmentActivity implements OnMapReadyC
         if (mDriverMarker != null) {
             mDriverMarker.remove();
         }
-        mRequest.setText("call Uber");
+        mRequest.setText("call Fahrer");
 
         mDriverInfo.setVisibility(View.GONE);
         mDriverName.setText("");
